6d5528caa8aa8a6e2e694c69310bb14ae51b80f0
Manish Tanwar <manish.tanwar19@gmail.com>
2026-05-27 23:48:43 +0530

    Enable python tool

--- init.lua
+++ init.lua
@@ -681,7 +681,7 @@ require('lazy').setup({
         -- kotlin_language_server = {},
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
